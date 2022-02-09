f06cc3ea91602507fec093ab8e9592a0be46c540
mehrarohit25 <99366145+mehrarohit25@users.noreply.github.com>
2022-02-09 21:47:14 +0000

    Initial project structure
    Project structure with bare minimum dependencies.

---
+++ src/main/java/com/publicis/sapient/creditcard/management/CreditCardManagementApplication.java
@@ -0,0 +1,13 @@
+package com.publicis.sapient.creditcard.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditCardManagementApplication.class, args);
+    }
+
+}
